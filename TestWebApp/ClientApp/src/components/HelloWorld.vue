97ade502d40a5e0738da009c457bfb5b8311cae1
drsmile <drsmile.1001@gmail.com>
2019-01-23 17:13:22 +0800

    加入前端專案

---
+++ TestWebApp/ClientApp/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hello">
+        <button @click="test">aaa</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  created(){
+    // eslint-disable-next-line
+    console.log(process.env.VUE_APP_BASE_URL || process.env.BASE_URL)
+  },
+  methods: {
+    test() {
+      axios
+        .get(
+          `${process.env.VUE_APP_BASE_URL || process.env.BASE_URL}api/values`
+        )
+        .then(response => {
+          // eslint-disable-next-line
+          console.log(response);
+        });
+    }
+  }
+};
+</script>
+
--- TestWebApp/Startup.cs
+++ TestWebApp/Startup.cs
@@ -25,7 +25,12 @@ namespace TestWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSpaStaticFiles(spa =>
+            {
+                spa.RootPath = @"ClientApp\dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +42,25 @@ namespace TestWebApp
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
             app.UseMvc();
+            app.UseSpa(spa =>
+            {
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:8080/");
+                }
+            });
         }
     }
 }
